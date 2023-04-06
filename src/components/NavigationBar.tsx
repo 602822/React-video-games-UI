c84a11d417862062d9b7ef81a863f8cbbf6b50e4
602822 <vefjeld@gmail.com>
2023-04-06 23:32:08 +0200

    kan nå sette nav baren til aktiv

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
 function NavigationBar() {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const handleclick = (index: number) => {
+    setSelectedIndex(index);
+  };
+
   return (
-    <ul className="nav">
+    <ul className="nav nav-pills">
       <li className="nav-item">
-        <a className="nav-link active" href="#">
+        <a
+          className={selectedIndex === 0 ? "nav-link active" : "nav-link"}
+          href="#"
+          onClick={() => handleclick(0)}
+        >
           Games
         </a>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">
+        <a
+          className={selectedIndex === 1 ? "nav-link active" : "nav-link"}
+          href="#"
+          onClick={() => handleclick(1)}
+        >
           Favourite Games
         </a>
       </li>
